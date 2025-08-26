71619f5615588a8324360c9b5371b6286a6a87d4
cruzrodas <129350774+cruzrodas@users.noreply.github.com>
2025-08-25 19:34:06 -0600

    Cambios 25/08/2025

--- ProyectoFarmaVita/obj/Debug/net9.0/ProyectoFarmaVita.AssemblyInfo.cs
+++ ProyectoFarmaVita/obj/Debug/net9.0/ProyectoFarmaVita.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFarmaVita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a393c9c4ea22b9dca07488a361e2c5df0f37ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7abf74fa62d400feba4c753a6cd22d767d7883d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFarmaVita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFarmaVita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
